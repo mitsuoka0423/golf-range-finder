38a1c42fac776a17735b5104e1e136b0ff28be31
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-10-19 21:31:24 +0900

    2020/10/19 21:31:24

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-let map, infoWindow;
+let map, markerCurrentPosition, markerTappedPosition;
+
+const defaultCenter = { lat: 35.649218, lng: 139.5255548 };
+let current = JSON.parse(JSON.stringify(defaultCenter));
+
+const distanceElement = document.getElementById("distance");
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     mapTypeId: "satellite",
-    center: { lat: 35.6809591, lng: 139.7673068 },
-    zoom: 18,
+    center: defaultCenter,
+    zoom: 19,
+  });
+  map.setTilt(0);
+  map.addListener("click", ({latLng}) => {
+    console.log("map is tapped!");
+    console.log({latLng});
+    
+    if (markerTappedPosition) {
+      markerTappedPosition.setMap(null);
+    }
+    markerTappedPosition = new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+
+    distanceElement.innerHTML = calcDistanceByYard(current, latLng);
   });
 
-  const marker = new google.maps.Marker({
-    position: { lat: 35.6809591, lng: 139.7673068 },
+  markerCurrentPosition = new google.maps.Marker({
+    position: defaultCenter,
     map: map,
   });
 
@@ -19,24 +39,24 @@ function initMap() {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        map.setCenter(pos);
-        marker.setPosition(pos);
+        // map.setCenter(pos);
+        // markerCurrentPosition.setPosition(pos);
+        // current = pos;
       },
       () => {
-        handleLocationError(true, infoWindow, map.getCenter());
+        handleLocationError(true);
       }
     );
   } else {
-    handleLocationError(false, infoWindow, map.getCenter());
+    handleLocationError(false);
   }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+function handleLocationError(browserHasGeolocation) {
+  const message = browserHasGeolocation ? "Error: The Geolocation service failed." : "Error: Your browser doesn't support geolocation.";
+  alert(message);
+}
+
+function calcDistanceByYard(current, target) {
+  return Math.random();
 }
