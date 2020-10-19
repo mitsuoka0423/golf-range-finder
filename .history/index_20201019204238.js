4698718fc98242e2f6daba16efc97187a4ec14d5
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-10-19 20:44:43 +0900

    2020/10/19 20:44:43

--- .history/index_20201019204238.js
+++
@@ -1,9 +0,0 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    defaultCenter: { lat: 35.6809591, lng: 150.644 },
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 18,
-  });
-}
--- .history/index_20201019204327.js
+++
@@ -1,9 +0,0 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    map
-    center: { lat: 35.6809591, lng: 139.7673068 },
-    zoom: 18,
-  });
-}
