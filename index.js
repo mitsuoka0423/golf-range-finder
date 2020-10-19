ea19181dc55d24088bf2f0646cf55a648c288d93
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-10-19 20:54:40 +0900

    2020/10/19 20:54:40

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-let map;
+let map, infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    mapTypeId: 'satellite',
+    mapTypeId: "satellite",
     center: { lat: 35.6809591, lng: 139.7673068 },
     zoom: 18,
   });
+
+  infoWindow = new google.maps.InfoWindow();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
